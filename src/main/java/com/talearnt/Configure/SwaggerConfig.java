941c1bd2db11c756574b20d204dd5bd280d18de3
Woong <jin02014@naver.com>
2024-10-17 03:07:36 +0900

    chore : Update Swagger Config
    - 확장 가능성을 위해 Components 변수로 변경
    - Swagger-ui 이름 및 내용 변경

--- src/main/java/com/talearnt/Configure/SwaggerConfig.java
+++ src/main/java/com/talearnt/Configure/SwaggerConfig.java
@@ -10,15 +10,19 @@ import org.springframework.context.annotation.Configuration;
 public class SwaggerConfig {
     @Bean
     public OpenAPI openAPI() {
+
+        Components components = new Components();
+
+
         return new OpenAPI()
-                .components(new Components())
+                .components(components)
                 .info(apiInfo());
     }
 
     private Info apiInfo() {
         return new Info()
-                .title("Springdoc 테스트")
-                .description("Springdoc을 사용한 Swagger UI 테스트")
+                .title("Talearnt API")
+                .description("Talearnt 개발자의 API 소통구간")
                 .version("1.0.0");
     }
 
