c2c9389eb486c0930914e4b3b36a879ce508c9ee
Ryu_woong <jin02014@naver.com>
2024-10-24 01:30:31 +0900

    refector : Update ErrorCode
    - 아이디 , 비밀번호 틀렸을 경우를 수정
    
    Ref : TALEARNT-57

--- src/main/java/com/talearnt/enums/ErrorCode.java
+++ src/main/java/com/talearnt/enums/ErrorCode.java
@@ -13,13 +13,13 @@ public enum ErrorCode {
     // 인증 관련 오류
     INVALID_TOKEN("401-AUTH-01", "올바르지 않은 인증 토큰입니다."),
     EXPIRED_TOKEN("401-AUTH-02", "인증 토큰이 만료되었습니다."),
-    INVALID_CREDENTIALS("401-AUTH-03", "잘못된 사용자 이름 또는 비밀번호입니다."),
+    INVALID_CREDENTIALS("401-AUTH-03", "아이디 또는 비밀번호를 확인해주세요!"),
     ACCESS_DENIED("403-AUTH-04", "접근이 거부되었습니다."),
 
     // 사용자 관련 오류
     USER_NOT_FOUND("404-USER-01", "해당 회원을 찾을 수 없습니다."),
     DUPLICATE_USER_ID("400-USER-02", "해당 아이디는 이미 존재합니다."),
-    INVALID_USER_INPUT("400-USER-03", "아이디 또는 비밀번호를 확인해주세요!"),
+    INVALID_USER_INPUT("400-USER-03", "입력된 사용자 정보가 유효하지 않습니다."),
     USER_SUSPENDED("403-USER-04", "이 계정은 정지 상태입니다."),
     DUPLICATE_USER_NICKNAME("400-USER-05", "해당 닉네임은 이미 존재합니다."),
 
